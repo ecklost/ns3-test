316200062e71fa511c9c6debfafa7c492e725fc0
Eric Klostermeyer <ecklost@users.noreply.github.com>
2016-02-02 15:22:05 -0500

    Update p1.cc
    Changed a few things. Not done..?

--- p1.cc
+++ p1.cc
@@ -17,12 +17,14 @@
 /*
  * TCP Source   10ms       20ms       10ms   TCP Sink
  *          O -------- O -------- O -------- O
- *              5Mb	   1Mb        5Mb
+ *              5Mb		   1Mb        5Mb
  *
  * The middlemost portion is a bottleneck link.
  * Goodput refers to data received at sink divided by simulation time.
  */
  
+using namespace ns3;
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -39,16 +41,12 @@
 #include "ns3/event-id.h"
 #include "ns3/ipv4-global-routing-helper.h"
 
-using namespace ns3;
-
-//NS_LOG_COMPONENT_DEFINE ("P1 Information");
+NS_LOG_COMPONENT_DEFINE ("P1 Information");
  
 int
 main (int argc, char *argv[])
 {
-  //NS_LOG_INFO ("Begin...");
   std::string transport_prot = "TcpTahoe";
-  Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TcpTahoe::GetTypeId ()));
   
   /*
    * Node List
@@ -57,81 +55,78 @@ main (int argc, char *argv[])
    * 2 : Bottleneck Exit
    * 3 : TCP Sink
    */
+  NS_LOG_INFO ("Node Creation");
+  NodeContainer source;
+  source.Create (1);
+  NodeContainer bottleneck;
+  bottleneck.Create (2);
+  NodeContainer sink;
+  sink.Create (1);
    
-   //NS_LOG_INFO ("Node Creation");
-   NodeContainer source;
-   source.Create (1);
-   NodeContainer bottleneck;
-   bottleneck.Create (2);
-   NodeContainer sink;
-   sink.Create (1);
+  NS_LOG_INFO ("Point-to-point Creation");
+  PointToPointHelper sourceLink;
+  sourceLink.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  sourceLink.SetChannelAttribute ("Delay", StringValue ("10ms"));
    
-   //NS_LOG_INFO ("Point-to-point Creation");
-   PointToPointHelper sourceLink;
-   sourceLink.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-   sourceLink.SetChannelAttribute ("Delay", StringValue ("10ms"));
+  PointToPointHelper bottleneckLink;
+  bottleneckLink.SetDeviceAttribute ("DataRate", StringValue ("1Mbps"));
+  bottleneckLink.SetChannelAttribute ("Delay", StringValue ("20ms"));
    
-   PointToPointHelper bottleneckLink;
-   bottleneckLink.SetDeviceAttribute ("DataRate", StringValue ("1Mbps"));
-   bottleneckLink.SetChannelAttribute ("Delay", StringValue ("20ms"));
+  PointToPointHelper sinkLink;
+  sinkLink.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  sinkLink.SetChannelAttribute ("Delay", StringValue ("10ms"));
    
-   PointToPointHelper sinkLink;
-   sinkLink.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-   sinkLink.SetChannelAttribute ("Delay", StringValue ("10ms"));
+  bottleneckLink.SetQueue ("DropTailQueue", 
+						   "Mode", StringValue (QUEUE_MODE_PACKETS), 
+						   "MaxPackets", UintegerValue (2000));
    
-   bottleneckLink.SetQueue ("ns3::DropTailQueue", 
-							"Mode", StringValue ("QUEUE_MODE_PACKETS"), 
-							"MaxPackets", UintegerValue (64000));
-
-   InternetStackHelper stack;
-   stack.InstallAll ();
+  InternetStackHelper stack;
+  stack.InstallAll ();
    
-   Ipv4AddressHelper address;
-   address.SetBase ("10.0.0.0", "255.255.255.0");
+  address.SetBase ("10.0.0.0", "255.255.255.0");
+  Ipv4AddressHelper address;
    
-   Ipv4InterfaceContainer sinkInterface;
+  Ipv4InterfaceContainer sinkInterface;
    
-   //NS_LOG_INFO ("Connecting nodes...");
-   NetDeviceContainer devices;
+  // Connecting nodes
+  NetDeviceContainer devices;
    
-   devices = sourceLink.Install (source.Get (0), bottleneck.Get (0));
-   address.NewNetwork ();
-   Ipv4InterfaceContainer interfaces = address.Assign (devices);
+  devices = sourceLink.Install (source.Get (0), bottleneck.Get (0));
+  address.NewNetwork ();
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+  devices = bottleneckLink.Install (bottleneck.Get (0), bottleneck.Get(1));
+  address.NewNetwork ();
+  interfaces = address.Assign (devices);
+  devices = sinkLink.Install (bottleneck.Get(1), sink.Get (0));
+  address.NewNetwork ();
+  interfaces = address.Assign (devices);
    
-   devices = bottleneckLink.Install (bottleneck.Get (0), bottleneck.Get(1));
-   address.NewNetwork ();
-   interfaces = address.Assign (devices);
+  sinkInterface.Add (interfaces.Get (1));
+  
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
    
-   devices = sinkLink.Install (bottleneck.Get(1), sink.Get (0));
-   address.NewNetwork ();
-   interfaces = address.Assign (devices);
-   
-   sinkInterface.Add (interfaces.Get (1));
-   
-   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-   
-   uint16_t sinkPort = 8080;
-   Address sinkLocalAddress (InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
-   PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", sinkLocalAddress);
+  uint16_t sinkPort = 50000;
+  Address sinkLocalAddress (InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
+  PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", sinkLocalAddress);
    
    // Configure app.
-   AddressValue remoteAddress (InetSocketAddress (sinkInterface.GetAddress (0, 0), sinkPort));
-   Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (512));
-   BulkSendHelper ftp ("ns3::TcpSocketFactory", Address ());
-   ftp.SetAttribute ("Remote", remoteAddress);
-   ftp.SetAttribute ("SendSize", UintegerValue (512));
-   ftp.SetAttribute ("MaxBytes", UintegerValue (0));
+  AddressValue remoteAddress (InetSocketAddress (sinkInterface.GetAddress (0, 0), port))
+  Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (128));
+  BulkSendHelper ftp ("ns3::TcpSocketFactory", Address ());
+  ftp.SetAttribute ("Remote", remoteAddress);
+  ftp.SetAttribute ("SendSize", UintegerValue (128));
+  ftp.SetAttribute ("MaxBytes", UintegerValue (100000000));
    
-   ApplicationContainer sourceApp = ftp.Install (source.Get (0));
-   sourceApp.Start (Seconds (0));
-   sourceApp.Stop (Seconds (7));
+  ApplicationContainer sourceApp = ftp.Install (source.Get (0));
+  sourceApp.Start (Seconds (0));
+  sourceApp.Stop  (Seconds (0.1));
    
-   sinkHelper.SetAttribute ("Protocol", TypeIdValue (TcpSocketFactory::GetTypeId ()));
-   ApplicationContainer sinkApp = sinkHelper.Install (sink);
-   sinkApp.Start (Seconds (0));
-   sinkApp.Stop  (Seconds (10));
+  sinkHelper.SetAttribute ("Protocol", TypeIdValue (TcpSocketFactory::GetTypeId ()));
+  ApplicationContainer sinkApp = sinkHelper.Install (sink);
+  sinkApp.Start (Seconds (0));
+  sinkApp.Stop  (Seconds (0.2));
    
-   Simulator::Run ();
-   Simulator::Destroy ();
-   return 0;
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
 }
